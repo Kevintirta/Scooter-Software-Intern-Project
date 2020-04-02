c27ffbf8bca6a64f1f08801ea630182b2670eaaa
Kevintirta <kevintirta1@yahoo.com>
2020-04-02 20:13:54 +0800

    change logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,10 +6,10 @@ const Header = () => {
     return (
         <div className="topnav">
             <div className="logo">
-                <p>beam</p>
+                <p>Logo</p>
             </div>
             <div className="navbar">
-                <a target="_blank" rel="noopener noreferrer" href="https://www.ridebeam.com/">Go to Beam</a>
+                <a target="_blank" rel="noopener noreferrer" href="https://www.google.com/">Go to Your Website</a>
                 <a target="_blank" rel="noopener noreferrer" href="https://firebasestorage.googleapis.com/v0/b/beam-scooter-locator.appspot.com/o/KevinGondokusumoResume.pdf?alt=media&token=813acd54-3c84-497d-979d-33f98ea604c4">Download my resume</a>
             </div>
         </div>
