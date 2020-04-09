494ec6f16f5895969f196e660bbb2014b5b3e58d
Kevintirta <kevintirta1@yahoo.com>
2020-04-09 21:52:22 +0800

    change direction

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
                 <p>Logo</p>
             </div>
             <div className="navbar">
-                <a target="_blank" rel="noopener noreferrer" href="https://www.google.com/">Go to Your Website</a>
+                <a target="_blank" rel="noopener noreferrer" href="https://www.google.com/">Go to Google</a>
                 <a target="_blank" rel="noopener noreferrer" href="https://firebasestorage.googleapis.com/v0/b/beam-scooter-locator.appspot.com/o/KevinGondokusumoResume.pdf?alt=media&token=813acd54-3c84-497d-979d-33f98ea604c4">Download my resume</a>
             </div>
         </div>
